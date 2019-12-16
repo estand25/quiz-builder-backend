5900d897f408712c5d5611ecc270f6deb0f608e7
Standley Eugene <estand25@gmail.com>
2019-12-15 23:13:26 -0500

    Back-end is working now

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const db = require('./db')
+
+const user = require('./routes/user-router')
+const quiz = require('./routes/quiz-router')
+const question = require('./routes/question-router')
+const userResponse = require('./routes/userResponse-router')
+const score = require('./routes/score-router')
+
 const app = express()
 const apiPost = 3000
 
@@ -9,8 +17,12 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!')
-})
+db.on('error', console.error.bind(console, 'MongoDB connection error '));
+
+app.use('/api', question)
+app.use('/api', quiz)
+app.use('/api', score)
+app.use('/api', user)
+app.use('/api', userResponse)
 
 app.listen(apiPost, () => console.log(`Server running on port ${apiPost}`))
